1d6ce0258396da748c9398139c5c1813ce6c4ebf
Hiroshi Yamada <hiroshi.yamada426@gmail.com>
2022-10-31 18:03:57 +0900

    Add hello app

---
+++ app.py
@@ -0,0 +1,8 @@
+from wsgiref.simple_server import make_server
+
+def app(environ, start_response):
+        start_response('200 OK', [('Content-Type', 'text/plain')])
+        return [b"Hello World, my name is Hiroshi.\n"]
+
+httpd = make_server('0.0.0.0', 8000, app)
+httpd.serve_forever()
